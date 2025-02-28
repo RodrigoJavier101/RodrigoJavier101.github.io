f849c9766e62c842c30096317ccb4aee5cb62982
rj <rj@gigioberto.com>
2025-02-28 17:17:53 -0300

    stable version

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,22 +1,21 @@
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
 };
 
 const routes = {
-    404: "/pages/404.html",
-    "/": "/pages/index.html",
-    "/about": "/pages/about.html",
-    "/lorem": "/pages/lorem.html",
+  404: "/pages/404.html",
+  "/content.html": "/pages/home-content.html",
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
+  const path = window.location.pathname;
+  console.log(path);
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
