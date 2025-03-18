23a830d1fddf5319a8275cb33423e3dfbcd87341
rj <rj@gigioberto.com>
2025-03-18 10:35:16 -0300

    sending to repos

--- assets/js/router.js
+++ assets/js/router.js
@@ -10,10 +10,16 @@
     404: "/pages/404.html",
     "/chunks.html": "/pages/chunks/home.html",
     "/chunks": "/pages/chunks/home.html",
+    "/maths": "/pages/maths/home.html",
+    "/science": "/pages/science/home.html",
+    "/computer-science": "/pages/computer-science/home.html",
   };
 
   const handleLocation = async () => {
     const path = window.location.pathname;
+
+    console.log(path, "EL CAMINO");
+
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-page").innerHTML = html;
