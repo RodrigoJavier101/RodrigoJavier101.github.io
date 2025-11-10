4c54d9f1e04ff1db4fb5c473a54b13f0550f9efb
rj <rj@rodrigojavier101.cl>
2025-11-09 21:15:50 -0300

    sending to repos

--- js/translations/pages/projects.const.js
+++ js/translations/pages/projects.const.js
@@ -5,19 +5,10 @@ export const projects = {
       title: "Docendo Discitur",
       description: "A multilingual educational platform for developers.",
       tech: ["JavaScript", "Firebase", "CSS"],
-      liveUrl: "https://rodrigojavier101.github.io",
+      liveUrl: "https://www.youtube.com/@DocendoDiscitur-EN",
       githubUrl:
         "https://github.com/RodrigoJavier101/rodrigojavier101.github.io",
       image: "assets/images/docendo-preview.png",
-    },
-    {
-      id: "code-chunks",
-      title: "Code Chunks",
-      description: "YouTube channel with coding tutorials in Spanish.",
-      tech: ["YouTube", "Video Editing", "JavaScript"],
-      liveUrl: "https://www.youtube.com/@DocendoDisciturEN",
-      githubUrl: null,
-      image: "assets/images/codechunks-preview.png",
     },
   ],
   es: [
@@ -26,20 +17,10 @@ export const projects = {
       title: "Docendo Discitur",
       description: "Plataforma educativa multilingüe para desarrolladores.",
       tech: ["JavaScript", "Firebase", "CSS"],
-      liveUrl: "https://rodrigojavier101.github.io",
+      liveUrl: "https://www.youtube.com/@DocendoDiscitur-ES",
       githubUrl:
         "https://github.com/RodrigoJavier101/rodrigojavier101.github.io",
       image: "assets/images/docendo-preview.png",
-    },
-    {
-      id: "code-chunks",
-      title: "Code Chunks",
-      description:
-        "Canal de YouTube con tutoriales de programación en español.",
-      tech: ["YouTube", "Edición de video", "JavaScript"],
-      liveUrl: "https://www.youtube.com/@DocendoDisciturES",
-      githubUrl: null,
-      image: "assets/images/codechunks-preview.png",
     },
   ],
 };
