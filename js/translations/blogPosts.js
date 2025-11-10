6f2a7525520ea221cf649082750d6855141fccf1
rj <rj@rodrigojavier101.cl>
2025-11-09 21:44:34 -0300

    ---------

--- js/translations/blogPosts.js
+++ js/translations/blogPosts.js
@@ -4,8 +4,7 @@ export const blogPosts = {
       id: "1",
       title: "Intro to YT Channels",
       date: "2025-11-09",
-      excerpt:
-        "An introduction to the YouTube channel.",
+      excerpt: "An introduction to the YouTube channel.",
       youtubeUrl: "https://www.youtube.com/watch?v=jNQXAC9IVRw", // 👈 añade esta línea
       content: `
        <p>Docendo Discitur, del latín enseñando, se aprende. 
@@ -28,13 +27,13 @@ Este esfuerzo de hacer videos explicativos lo hago con la meta de mejorar person
       youtubeUrl: "https://www.youtube.com/watch?v=jNQXAC9IVRw",
       content: `
         <p>Docendo Discitur, del latín enseñando, se aprende. 
-Este esfuerzo de hacer videos explicativos lo hago con la meta de mejorar personalmente en el conocimiento mediante la técnica de intentar enseñar.
-</p>
-<p>La expresión la tomé de la frase de Séneca de sus Cartas Morales a Lucilius, Carta 7, sección 8, la que voy a citar expresamente: “…no debes imitar lo malo solo porque sea abundante, ni odiar a la mayoría porque sea diferente a ti. Retírate a tu interior, tanto como puedas. Rodéate de quienes te ayuden a ser mejor persona. Acoge a quienes puedas mejorar. El proceso es mutuo; pues se aprende enseñando”, y en latín Processus est mutuus; discimus docendo.</p>
-<p>El científico Richard Feynman tuvo el principio de enseñar a los principiantes sobre temas avanzados haciendo conferencias y dando clases a los primerizos. Así que, humildemente, imaginaré que hago el papel de profesor para mí mismo.</p>
-<p>Las temáticas que intentaré abordar son especialmente las relacionadas con informática y, yendo un poco más allá, me gustaría desviarme un de la informática y avanzar un poco en temas científicos en general tal como matemáticas y otros semejantes. Me gustaría ser lo más específico y lo más acotado posible en relación a los temas que estaría abordando sin el objetivo final de abarcar todo de todo.</p>
-<p>Así que espero, de ahora en adelante, imprimir mi óptica de las cosas y aprender, pero debatiendo conmigo mismo intentando operar en mi propia mente.</p>
-<p>Salut!</p>
+          Este esfuerzo de hacer videos explicativos lo hago con la meta de mejorar personalmente en el conocimiento mediante la técnica de intentar enseñar.
+          </p>
+          <p>La expresión la tomé de la frase de Séneca de sus Cartas Morales a Lucilius, Carta 7, sección 8, la que voy a citar expresamente: “…no debes imitar lo malo solo porque sea abundante, ni odiar a la mayoría porque sea diferente a ti. Retírate a tu interior, tanto como puedas. Rodéate de quienes te ayuden a ser mejor persona. Acoge a quienes puedas mejorar. El proceso es mutuo; pues se aprende enseñando”, y en latín Processus est mutuus; discimus docendo.</p>
+          <p>El científico Richard Feynman tuvo el principio de enseñar a los principiantes sobre temas avanzados haciendo conferencias y dando clases a los primerizos. Así que, humildemente, imaginaré que hago el papel de profesor para mí mismo.</p>
+          <p>Las temáticas que intentaré abordar son especialmente las relacionadas con informática y, yendo un poco más allá, me gustaría desviarme un de la informática y avanzar un poco en temas científicos en general tal como matemáticas y otros semejantes. Me gustaría ser lo más específico y lo más acotado posible en relación a los temas que estaría abordando sin el objetivo final de abarcar todo de todo.</p>
+          <p>Así que espero, de ahora en adelante, imprimir mi óptica de las cosas y aprender, pero debatiendo conmigo mismo intentando operar en mi propia mente.</p>
+        <p>Salut!</p>
       `,
     },
   ],
