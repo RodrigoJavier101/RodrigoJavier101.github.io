a22c8761029a98fa93b3445a05518a63f1c8573c
rj <rj@rodrigojavier101.cl>
2026-01-19 02:54:19 -0300

    sending to repos

--- js/contact/contact.js
+++
@@ -1,9 +0,0 @@
-import { translations } from "../translations/translations.js";
-
-export const contact = (lang) => `
-  <div class="page container">
-    <h2>${translations[lang].contact_title}</h2>
-    <p>rodrigojaviergd@gmail.com</p>
-    <p>WSP: +56 996 095 384</p>
-  </div>
-`;
