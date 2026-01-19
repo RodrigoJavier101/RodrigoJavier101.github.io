f9900aaa0227ed402742bb6d6dba38e24ed3e714
rj <rj@rodrigojavier101.cl>
2026-01-19 02:42:26 -0300

    ---------

--- js/home/home.js
+++ js/home/home.js
@@ -4,6 +4,12 @@ export const home = (lang) => `
   <div class="page container">
     <h1>${translations[lang].home_title}</h1>
     <p>${translations[lang].home_desc}</p>
-    <p>${translations[lang].home_body || ''}</p>
+    <p>${translations[lang].home_body || ""}</p>
+  </div>
+  
+  <div class="page container">
+    <h2>${translations[lang].contact_title}</h2>
+    <p>rodrigojaviergd@gmail.com</p>
+    <p>WSP: +56 996 095 384</p>
   </div>
 `;
