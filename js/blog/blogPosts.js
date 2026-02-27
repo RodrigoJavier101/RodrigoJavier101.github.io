61d17d78efbc7a1fb2a26ca35ede2d1d9a51565a
rj <rj@rodrigojavier101.cl>
2026-02-26 21:22:36 -0300

    sending to repos

--- js/blog/blogPosts.js
+++ js/blog/blogPosts.js
@@ -9,13 +9,13 @@ export const blogPosts = {
       date: formatearFecha("2025-09-11", langs.en).complete,
       youtubeUrl: "https://www.youtube.com/watch?v=jNQXAC9IVRw",
       content: `
-      <p><strong>Docendo Discitur</strong>, from the Latin "by teaching, one learns." </p>
-      <p>I make this effort to create explanatory videos with the goal of personally improving my knowledge through the technique of trying to teach. </p>
-      <p>I took the expression from Seneca's Moral Letters to Lucilius, Letter 7, section 8, which I will quote explicitly: “…you should not imitate what is bad simply because it is abundant, nor hate the majority because they are different from you. Withdraw into yourself as much as you can. Surround yourself with those who help you become a better person. Welcome those you can improve. <strong>The process is mutual; for one learns by teaching,” (in Latin, Processus est mutuus; discimus docendo)</strong>. </p>
-      <p>The scientist Richard Feynman had the principle of teaching beginners on advanced topics by giving lectures and classes to those new to the subject. So, humbly, I will imagine myself playing the role of teacher to myself. </p>
-      <p>The topics I will try to cover are primarily related to computer science, and, going a bit further, I'd like to venture beyond computer science and delve into general scientific subjects such as mathematics and similar fields. I'd like to be as specific and focused as possible regarding the topics I address, without aiming to cover everything. </p>
-      <p>So, from now on, I hope to share my perspective and learn, but by engaging in internal debate and working through my own thinking. </p>
-      <p>Salut!</p>
+        <p><strong>Docendo Discitur</strong>, from the Latin "by teaching, one learns." </p>
+        <p>I make this effort to create explanatory videos with the goal of personally improving my knowledge through the technique of trying to teach. </p>
+        <p>I took the expression from Seneca's Moral Letters to Lucilius, Letter 7, section 8, which I will quote explicitly: “…you should not imitate what is bad simply because it is abundant, nor hate the majority because they are different from you. Withdraw into yourself as much as you can. Surround yourself with those who help you become a better person. Welcome those you can improve. <strong>The process is mutual; for one learns by teaching,” (in Latin, Processus est mutuus; discimus docendo)</strong>. </p>
+        <p>The scientist Richard Feynman had the principle of teaching beginners on advanced topics by giving lectures and classes to those new to the subject. So, humbly, I will imagine myself playing the role of teacher to myself. </p>
+        <p>The topics I will try to cover are primarily related to computer science, and, going a bit further, I'd like to venture beyond computer science and delve into general scientific subjects such as mathematics and similar fields. I'd like to be as specific and focused as possible regarding the topics I address, without aiming to cover everything. </p>
+        <p>So, from now on, I hope to share my perspective and learn, but by engaging in internal debate and working through my own thinking. </p>
+        <p>Salut!</p>
       `,
     },
   ],
