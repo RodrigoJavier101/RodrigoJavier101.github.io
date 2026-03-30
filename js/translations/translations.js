531405ba1655359c30df5662d19f785fcbc5f407
rj <rj@rodrigojavier101.cl>
2026-03-30 16:46:49 -0300

    ---------

--- js/translations/translations.js
+++ js/translations/translations.js
@@ -6,92 +6,107 @@ export const translations = {
   home_title: {
     en: "Hi, my name is Rodrigo Javier, Software Engineer.",
     es: "Hola, mi nombre es Rodrigo Javier, Ingeniero de Software.",
-    // il: "שלום, שמי רודריגו חאווייר, מהנדס תוכנה." // ✅ Fácil de agregar
+    il: "שלום, שמי רודריגו חוויאר, מהנדס תוכנה"
   },
   
   home_body: {
     en: `Please, pay attention to the <strong>blog</strong> section and the <strong>projects</strong> that I'm working on.`,
     es: `Por favor, presta atención a la sección de <strong>blog</strong> y a los <strong>proyectos</strong> en los que he trabajado.`,
-    // il: `...`,
+    il: 'אנא שימו לב לאזור ה<strong>בלוג</strong> ול<strong>פרויקטים</strong> עליהם עבדתי',
   },
 
   // Blog
   blog_title: {
     en: "Articles",
     es: "Artículos",
-    // il: "מאמרים",
+    il: "מאמרים",
   },
 
   // Projects
   projects_title: {
     en: "Featured Projects",
     es: "Proyectos Destacados",
+    il: "פרויקטים נבחרים"
   },
 
   // Contact
   contact_emailme: {
     en: "Email me:",
     es: "Escríbeme:",
+    il: "כתבו לי"
   },
   
   contact_email: {
     en: "rodrigojaviergd@gmail.com",
     es: "rodrigojaviergd@gmail.com", // Same email
+    il: "rodrigojaviergd@gmail.com", // Same email
   },
   
   contact_phoneme: {
     en: "WhatsApp",
     es: "WhatsApp",
+    il: "WhatsApp",
   },
   
   contact_phone: {
     en: "+56 996 095 384",
     es: "+56 996 095 384",
+    il: "+56 996 095 384",
   },
   
   contact_alt: {
-    en: "Contact:",
+    en: "Contact information:",
     es: "Medios de contacto:",
+    il: "פרטי קשר",
   },
 
   // Navigation
   nav_home: {
     en: "Home",
     es: "Inicio",
+    il: "הַתחָלָה",
   },
   nav_projects: {
     en: "Projects",
     es: "Proyectos",
+    il: "פרויקטים",
   },
   nav_blog: {
     en: "Blog",
     es: "Blog",
+    il: "בלוג",
   },
   nav_contact: {
     en: "Contact",
     es: "Contacto",
+    il: "מַגָע",
   },
   nav_docendo: {
     en: "DocendoDiscitur",
     es: "DocendoDiscitur",
+    il: "DocendoDiscitur",
   },
 
   // Downloads
   download_cv: {
     en: "Resume",
     es: "Currículum",
+    il: "קוֹרוֹת חַיִים",
   },
   download_eng_cert: {
     en: "Eng Cert",
     es: "Cert de Inglés",
+    il: "תעודת אנגלית",
   },
   download_facil_cert: {
     en: "Facil Cert",
     es: "Cert de Facilitador",
+    il: "תעודת מנחה",
   },
   download_boot_cert: {
     en: "Boot Cert",
     es: "Cert de Bootcamp",
+    il: "תעודת אימונים",
   },
 };
 
@@ -99,4 +114,4 @@ export const translations = {
 export const DEFAULT_LANG = "en";
 
 // ✅ Lista de idiomas soportados
-export const SUPPORTED_LANGS = ["en", "es"]; // Agrega "il" cuando lo necesites
+export const SUPPORTED_LANGS = ["en", "es", "il"]; 
